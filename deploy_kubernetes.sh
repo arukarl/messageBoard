d5998e1b27666ec9f359002d0da423dbd957797e
KarlAru <kiri@karlaru.ee>
2022-07-27 13:17:17 +0300

    Assume iam role

--- deploy_kubernetes.sh
+++ deploy_kubernetes.sh
@@ -10,8 +10,6 @@ kops validate cluster --wait 15m
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.2.0/deploy/static/provider/aws/deploy.yaml
 
-kubectl create secret generic aws-credentials --from-file=../secrets/boto.cfg
-
 kubectl create secret tls main-tls --key="../secrets/SectigoSSL.key" --cert="../secrets/SectigoSSL.crt"
 
 kubectl create secret generic flask-conf --from-file=../message-board/conf.cfg
@@ -24,8 +22,8 @@ echo "Waiting 60s before appling ingress"
 sleep 60
 kubectl apply -f ingress.yaml
 
-echo "Waiting 5 min to get load balancer URL"
-sleep 300
+echo "Waiting 60s to get load balancer URL"
+sleep 60
 nlb="$(kubectl get ingress -o=jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}')"
 
 echo "$nlb"
