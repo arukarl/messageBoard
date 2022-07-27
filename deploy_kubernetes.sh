214bdfc4e174fa0a60ebf096f7165693853c6d82
KarlAru <kiri@karlaru.ee>
2022-07-27 03:27:29 +0300

    Security

--- deploy_kubernetes.sh
+++ deploy_kubernetes.sh
@@ -12,12 +12,20 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/cont
 
 kubectl create secret generic aws-credentials --from-file=../secrets/boto.cfg
 
-kubectl create secret tls main-tls --key="../secrets/SectigoSSL.key" --cert="./secrets/SectigoSSL.crt"
+kubectl create secret tls main-tls --key="../secrets/SectigoSSL.key" --cert="../secrets/SectigoSSL.crt"
 
 kubectl create secret generic flask-conf --from-file=../message-board/conf.cfg
 
 kubectl apply -f deployment.yaml
 
-echo "Waiting 30s before appling ingress"
-sleep 30
+kubectl apply -f deployment-docs.yaml
+
+echo "Waiting 60s before appling ingress"
+sleep 60
 kubectl apply -f ingress.yaml
+
+echo "Waiting 5 min to get load balancer URL"
+sleep 300
+nlb="$(kubectl get ingress -o=jsonpath='{.items[0].status.loadBalancer.ingress[0].hostname}')"
+
+echo "$nlb"
